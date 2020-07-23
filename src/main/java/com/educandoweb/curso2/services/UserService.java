ece4adacbe90fa576ee4c7b503a4138236ce56fe
André Demori <andre_demori@hotmail.com>
2020-07-23 18:50:20 -0300

    User update

--- src/main/java/com/educandoweb/curso2/services/UserService.java
+++ src/main/java/com/educandoweb/curso2/services/UserService.java
@@ -1,13 +1,13 @@
 package com.educandoweb.curso2.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.curso2.entities.User;
 import com.educandoweb.curso2.repositories.UserRepository;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -32,4 +32,15 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
